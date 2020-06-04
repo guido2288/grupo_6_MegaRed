15ff5e6bd63d3d05e670d577efebdf3bea4285ca
guidorocchio <guido.rocchio@gmail.com>
2020-06-03 22:50:19 -0300

    solucione problemas para leer JSON

--- Proyecto MegaRed/controllers/productsControllers.js
+++ Proyecto MegaRed/controllers/productsControllers.js
@@ -3,9 +3,9 @@ const fs = require("fs");
 
 let productsControllers = {
     products : function (req, res){
-
-
-        res.render("products")
+        let productos = JSON.parse(fs.readFileSync("./data/products.json" , {encoding: "utf-8"}));
+        console.log(productos)
+        res.render("products", {productos:productos})
     }
 
 
