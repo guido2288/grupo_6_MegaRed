d0b71b765c3d1b7bfb8b0609bd941c4c346910ff
guidorocchio <guido.rocchio@gmail.com>
2020-06-04 19:39:11 -0300

    envio datos a la vista productos

--- Proyecto MegaRed/controllers/productsControllers.js
+++ Proyecto MegaRed/controllers/productsControllers.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 let productsControllers = {
     products : function (req, res){
         let productos = JSON.parse(fs.readFileSync("./data/products.json" , {encoding: "utf-8"}));
-        console.log(productos)
+       // console.log(productos)
         res.render("products", {productos:productos})
     }
 
