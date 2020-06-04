4ecd202ffe5c933894846a9c41bc0ac38865d7d4
guidorocchio <guido.rocchio@gmail.com>
2020-06-03 22:14:21 -0300

    agrego productStyles

--- Proyecto MegaRed/app.js
+++ Proyecto MegaRed/app.js
@@ -25,7 +25,7 @@ app.use(methodOverride("_method"));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/products', productsRouter)
+app.use('/products', productsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- Proyecto MegaRed/controllers/productsControllers.js
+++ Proyecto MegaRed/controllers/productsControllers.js
@@ -3,12 +3,6 @@ const fs = require("fs");
 
 let productsControllers = {
     products : function (req, res){
-        //leo archivo products.json
-        let archivoUsers = fs.readFileSync("../data/products.json", {encoding: "utf-8"});
-        if(!archivoUsers){
-            res.send("ERROR")
-        }
-
 
 
         res.render("products")
