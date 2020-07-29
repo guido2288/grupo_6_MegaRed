ff3082b6c5a1846767981373c2c24fe15ed45f04
guidorocchio <guido.rocchio@gmail.com>
2020-07-29 00:16:36 -0300

    subo validaciones del lado del front, falta validar formulario de modificacion de productos del lado del BACK, y modificar estetica

---
+++ Proyecto MegaRed/public/js/cargaProducto.js
@@ -0,0 +1,83 @@
+
+window.onload = () => {
+    
+    let submit = document.querySelector("#submit");
+    let reset = document.querySelector("#reset");
+    let formCarga = document.querySelector("#formCarga");
+
+
+        reset.onclick = function(event){
+        var confirmacion = confirm("Estas seguro que deseas borrar todo? ")    
+            if(!confirmacion){
+                event.preventDefault();
+            }
+
+        }
+
+        formCarga.onsubmit = function(event){
+
+            let nombre = formCarga.querySelector("#nombre");
+            let descripcion = formCarga.querySelector("#descripcion");
+            let genre = formCarga.querySelector("#genre");
+            let platform = formCarga.querySelector("#platform");
+            let precio = formCarga.querySelector("#precio");
+            let stock = formCarga.querySelector("#stock");
+            let adjuntarimagen = formCarga.querySelector("#adjuntarimagen");
+           
+           
+            if(nombre.value.length<5){
+                formCarga.querySelector("#errorEmail").innerText= "Debes completar el nombre";
+                nombre.style.backgroundColor="#FF655D";
+                event.preventDefault();
+            };
+            if(descripcion.value.length<50){
+                formCarga.querySelector("#errorDescripcion").innerText= "La descripcion debe tener al menos 50 caracteres";
+                descripcion.style.backgroundColor="#FF655D";
+                event.preventDefault();
+            };
+            
+            if(!genre.value){
+                formCarga.querySelector("#errorGenero").innerText= "Debes seleccionar un genero";
+                genre.style.backgroundColor="#FF655D";
+                event.preventDefault();
+            };
+            if(!platform.value){
+                formCarga.querySelector("#errorPlatform").innerText= "Debes seleccionar un genero";
+                platform.style.backgroundColor="#FF655D";
+                event.preventDefault();
+            };
+            if(!precio.value){
+                formCarga.querySelector("#errorPrecio").innerText= "Debes completar el precio";
+                precio.style.backgroundColor="#FF655D";
+                event.preventDefault();
+            };
+            if(!stock.value){
+                formCarga.querySelector("#errorStock").innerText= "Debes completar el stock";
+                stock.style.backgroundColor="#FF655D";
+                event.preventDefault();
+            };
+            
+            if(!adjuntarimagen.value){
+                formCarga.querySelector("#erroradjuntarimagen").innerText= "Debes seleccionar una imagen";
+                adjuntarimagen.style.backgroundColor="#FF655D";
+                event.preventDefault();
+            };
+
+            
+            if( /\.(jpe?g|png|gif)$/i.test(adjuntarimagen.files[0].name) === false ){ 
+                formCarga.querySelector("#erroradjuntarimagen").innerText= "los formatos admitidos son jpg. png. jpeg.";
+                adjuntarimagen.style.backgroundColor="#FF655D";
+                event.preventDefault();
+              
+            };
+
+            
+
+
+
+
+
+        }
+
+
+}
